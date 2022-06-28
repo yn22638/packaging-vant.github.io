a2abac8ec95943ceaea07f36268ced4a87fe81b2
matthew <mxy@spacetech.com.cn>
2022-06-28 14:36:21 +0800

    fix: router

--- src/router/index.ts
+++ src/router/index.ts
@@ -116,9 +116,9 @@ const routes: Array<RouteRecordRaw> = [
 ]
 const router = createRouter({
   // 设置为history模式，就是路径里面没有#,  createWebHashHistory 是默认的，括号里面的就是基础路径，可以理解为项目名称，就是请求路径的基础url
-  history: createWebHistory(),
+  // history: createWebHistory(),
   // 此处打包以后http-serve 会报页面404，但是不设置/yn/就没问题
-  // history: createWebHistory('/yn/'),
+  history: createWebHistory('/packaging-vant.github.io/'),
   routes,
 })
 
