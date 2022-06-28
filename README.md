ad4b68d1b287d01d06aaa9580b75915f9041d450
yn22638 <yn22638@users.noreply.github.com>
2022-06-28 02:08:57 +0000

    deploy: 561131c9d9e34cf1b4a8264e23ca7a818c64955f

--- README.md
+++
@@ -1,2 +0,0 @@
-# packaging-vant.github.io
-主要基于 vant3 有些组件做了二次封装
---
+++ use-refs.caf8d3bb.js
@@ -0,0 +1,1 @@
+import{A as a,aL as e}from"./index.dd7c45e3.js";function n(){const n=a([]),o=[];e((()=>{n.value=[]}));return[n,a=>(o[a]||(o[a]=e=>{n.value[a]=e}),o[a])]}export{n as u};
