0f5ae7476b9bacff23dcb37e56a9178e2ff79880
Yn_22638 <1941263063@qq.com>
2022-06-30 20:50:45 +0800

    feat(module): 组件实例模板

---
+++ src/views/model/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="min-h-full xxx__demo">
+    <div class="px-16px pt-24px">
+      <p class="font-bold  text-18px xxx__title">m-</p>
+      <p class="pt-12px pb-16px text-13px leading-20px xxx__introduce ">介绍：
+      </p>
+    </div>
+    <van-collapse v-model="activeNames" accordion @change="onClickChange">
+      <van-collapse-item title="默认加载" name="1">
+        <p></p>
+        <van-button type="primary" size="mini" @click="onClick">click</van-button>
+      </van-collapse-item>
+    </van-collapse>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const activeNames = ref('1')
+
+// collapse click
+const onClickChange = (index: number) => {
+  activeNames.value = `${index}`
+}
+
+const onClick = () => { }
+</script>
+
+<style lang="less" scoped>
+</style>
