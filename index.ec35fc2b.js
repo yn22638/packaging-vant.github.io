6a3700c86464cb812037caf9bbfb7cc130b6b361
yn22638 <yn22638@users.noreply.github.com>
2022-07-03 09:04:20 +0000

    deploy: 32b6d05dfca98adee3b5a48a13cf4846a02563c9

--- index.ec35fc2b.js
+++
@@ -1,1 +0,0 @@
-import{g as s,o as a,c as e,a as l}from"./index.24ac1391.js";const p={class:"flex p-20px justify-between"},t=[l("p",null,"今天：",-1),l("p",null,"吃什么！",-1)],n=s({setup:s=>(s,l)=>(a(),e("div",p,t))});export{n as default};
---
+++ use-refs.20f66f91.js
@@ -0,0 +1,1 @@
+import{A as a,aL as e}from"./index.704aa62d.js";function n(){const n=a([]),o=[];e((()=>{n.value=[]}));return[n,a=>(o[a]||(o[a]=e=>{n.value[a]=e}),o[a])]}export{n as u};
