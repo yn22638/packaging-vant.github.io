962921cb304b57becbb919872d38c84e1e3e0931
yn22638 <yn22638@users.noreply.github.com>
2022-07-13 09:20:10 +0000

    deploy: 0623e67404e9c135e0714d93d7bbffb19c1c081e

---
+++ V3Proxy.f98538df.js
@@ -0,0 +1,1 @@
+import{g as e,C as t,o,c as s}from"./index.a1c71987.js";const a={class:""},r=e({setup(e){t((()=>{setTimeout((()=>{document.body.innerHTML="hello vue3"}),1e3)}));return new Proxy({text:"hello world"},{get:(e,t)=>e[t],set:(e,t,o)=>(e[t]=o,!0)}),(e,t)=>(o(),s("div",a))}});export{r as default};
