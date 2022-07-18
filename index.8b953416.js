9353a0f658f8139b23fa7643d4b1319ca66f2a51
yn22638 <yn22638@users.noreply.github.com>
2022-07-18 07:28:39 +0000

    deploy: 9e2ab7250240a544f3e3982b6feb0559c6fc085e

---
+++ index.8b953416.js
@@ -0,0 +1,1 @@
+import{g as s,o as a,c as e,a as l}from"./index.bf20c28a.js";const p={class:"flex p-20px justify-between"},t=[l("p",null,"今天：",-1),l("p",null,"吃什么！",-1)],f=s({setup:s=>(s,l)=>(a(),e("div",p,t))});export{f as default};
