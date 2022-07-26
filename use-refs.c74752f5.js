6044cfd82fca84660dcb68c22ae9646185536545
yn22638 <yn22638@users.noreply.github.com>
2022-07-26 02:56:42 +0000

    deploy: 46032649f4902d9e9bf913d6fa45431f9cd280cc

---
+++ use-refs.c74752f5.js
@@ -0,0 +1,1 @@
+import{r as a,aL as e}from"./index.2977117e.js";function r(){const r=a([]),n=[];e((()=>{r.value=[]}));return[r,a=>(n[a]||(n[a]=e=>{r.value[a]=e}),n[a])]}export{r as u};
--- use-refs.d76a2fb7.js
+++
@@ -1,1 +0,0 @@
-import{A as a,aL as e}from"./index.e6a88f2c.js";function n(){const n=a([]),o=[];e((()=>{n.value=[]}));return[n,a=>(o[a]||(o[a]=e=>{n.value[a]=e}),o[a])]}export{n as u};
